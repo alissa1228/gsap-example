b362fb8b7653510ccb65e989175b64872a871461
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-06-29 17:26:21 +0900

    #1 자식요소들 모든 선택 후 동일 애니메이션 적용

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import React, {useRef, useEffect} from "react";
 import { gsap } from "gsap";
 import './App.css';
 
+const Box = ({children}) => {
+  return(
+    <div className="box">{children}</div>
+  )
+}
+
+const Container = ({children}) => {
+  return(
+    <div><Box>Nested Box</Box></div>
+  )
+}
+
 function App() {
-  const boxRef = useRef();
+  const boxes = useRef();
+  const q = gsap.utils.selector(boxes)
 
   useEffect(()=> {
-    gsap.to(boxRef.current, {translateX: '50%'})
+    gsap.to(q(".box"), {
+      x:100,
+      stagger:0.33,
+      repeat: -1, // 반복
+      repeatDelay: 1,
+      yoyo: true //나갔다가 다시 부드럽게 돌아옴
+    })
   });
 
   return (
-    <div className="app" ref={boxRef}>
-     <div className="box">Hello</div>
+    <div className="app" ref={boxes}>
+    <Box>Box</Box>
+    <Container/>
+    <Box>Box2</Box>
    </div>
   );
 }
