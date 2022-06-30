f46d3f6e4003bf63b9dfb714870383c6cefc6f4b
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-06-30 14:25:05 +0900

    #1 props 내려서 state 변화에 따라 컨트롤할 수 있게

--- src/App.js
+++ src/App.js
@@ -2,57 +2,30 @@ import React, {useRef, useEffect, useState} from "react";
 import { gsap } from "gsap";
 import './App.css';
 
-const Box =({children}) => {
-  return(
-    <div className="box">{children}</div>
-  )
-};
 
+const randomX = gsap.utils.random(-200, 200, 1, true);
+//랜덤 숫자 받아서 Gsap x 쪽에 해당 값 넣기 -> 값 만큼 x 값 이동.
+const Box = ({ children, endX })=>  {    
+  const boxRef = useRef();
+
+  // run when `endX` changes
+  useEffect(() => {
+    gsap.to(boxRef.current, {
+      x: endX
+    });
+  }, [endX]);
+  
+  return <div className="box purple" ref={boxRef}>{children}</div>;
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [delayedCount, setDelayedCount] = useState(0);
-  //select 용도
-  const element = useRef();
-  const q = gsap.utils.selector(element);
-
-  //처음 렌더됐을 때만
-  useEffect(()=>{
-    gsap.to(q(".box-1"), {rotation: "+=360"});
-  },[]);
-
-  //맨 처음 랜더 + delayedCount 바뀔 때마다
-  useEffect(()=>{
-    gsap.to(q(".box-2"), {rotation: '+=360'})
-  },[delayedCount]);
-
-  //렌더 될 때마다
-  useEffect(()=>{
-    gsap.to(q(".box-3"), { rotation: "+=360" });
-  });
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setDelayedCount(count);
-    }, 1000);
-
-    return () => clearTimeout(timer);
-  }, [count]);
-
+  //랜덤 숫자 넣기.
+  const [endX, setEndX] = useState(0);
 
   return (
-    <div className="app" ref={element}>
-      <div>
-        <button className="toggle" onClick={() => setCount(count + 1)}>Click to trigger a render</button>
-      </div>
-      <p>Count: {count}</p>
-      <p>Delayed Count: {delayedCount}</p>
-      <p>Renders: {1 + delayedCount + count}</p>
-      <div className="flex-row">
-        <div className="box box-1 purple">First render</div>
-        <div className="box box-2 blue">First render & delayed count change</div>
-        <div className="box box-3 red">Every render</div>
-      </div>
+    <div className="app">
+      <button className="toggle" onClick={() => setEndX(randomX())}>Pass in a randomized value</button>
+      <Box endX={endX}>{endX}</Box>
     </div>
   );
 }
