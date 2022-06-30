641034175cc020f575bd7c4b8fc7790fde5605b5
mac1228 <68986081+alissa1228@users.noreply.github.com>
2022-06-30 12:58:15 +0900

    #1 react에서 애니메이션 컨트롤링

--- src/App.js
+++ src/App.js
@@ -8,49 +8,51 @@ const Box =({children}) => {
   )
 };
 
-const Container = ({children}) => {
-  return(
-    <div><Box>Nested Box</Box></div>
-  )
-}
-
-function Circle({ children }) {
-  return <div className="circle">{children}</div>;
-}
 
 function App() {
-  const [reversed, setReversed] = useState(false);
+  const [count, setCount] = useState(0);
+  const [delayedCount, setDelayedCount] = useState(0);
   //select 용도
   const element = useRef();
   const q = gsap.utils.selector(element);
 
-  //ref에 있는 타임라인 저장하기
-  const tl = useRef();
-  console.log(tl);
-
+  //처음 렌더됐을 때만
   useEffect(()=>{
-    tl.current = gsap.timeline()
-    .to(q(".box"),{
-      rotate: 360
-    })
-    .to(q(".circle"),{
-      x:100
-    })
+    gsap.to(q(".box-1"), {rotation: "+=360"});
   },[]);
 
+  //맨 처음 랜더 + delayedCount 바뀔 때마다
   useEffect(()=>{
-    //토글(우리 타임라인에서의 방향)
-    tl.current.reversed(reversed);
-  },[reversed])
+    gsap.to(q(".box-2"), {rotation: '+=360'})
+  },[delayedCount]);
+
+  //렌더 될 때마다
+  useEffect(()=>{
+    gsap.to(q(".box-3"), { rotation: "+=360" });
+  });
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDelayedCount(count);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [count]);
 
 
   return (
     <div className="app" ref={element}>
       <div>
-        <button className="toggle" onClick={() => setReversed(!reversed)}>Toggle</button>
+        <button className="toggle" onClick={() => setCount(count + 1)}>Click to trigger a render</button>
       </div>
-      <Box>box</Box>
-      <Circle>circle</Circle>
+      <p>Count: {count}</p>
+      <p>Delayed Count: {delayedCount}</p>
+      <p>Renders: {1 + delayedCount + count}</p>
+      <div className="flex-row">
+        <div className="box box-1 purple">First render</div>
+        <div className="box box-2 blue">First render & delayed count change</div>
+        <div className="box box-3 red">Every render</div>
+      </div>
     </div>
   );
 }
